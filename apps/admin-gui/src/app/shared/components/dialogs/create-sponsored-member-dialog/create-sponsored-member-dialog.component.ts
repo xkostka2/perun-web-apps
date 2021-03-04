a510ff763ad83d02baa69872a9ef6843c33ee253
Adam Bodnár <adam.bodnar10@gmail.com>
2021-03-04 09:38:06 +0100

    Support different namespaces for sponsorship
    * supported namespaces are now loaded from configuration
    * 'empty' namespace must be allowed in the instance config

--- apps/admin-gui/src/app/shared/components/dialogs/create-sponsored-member-dialog/create-sponsored-member-dialog.component.ts
+++ apps/admin-gui/src/app/shared/components/dialogs/create-sponsored-member-dialog/create-sponsored-member-dialog.component.ts
@@ -2,11 +2,12 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import {
   InputCreateSponsoredMember,
-  MembersManagerService, RichMember
+  MembersManagerService, NamespaceRules, RichMember
 } from '@perun-web-apps/perun/openapi';
 import { StoreService } from '@perun-web-apps/perun/services';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, ValidatorFn, Validators } from '@angular/forms';
 import { formatDate } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 
 export interface CreateSponsoredMemberDialogData {
   entityId?: number
@@ -29,6 +30,9 @@ export class CreateSponsoredMemberDialogComponent implements OnInit {
   createdMember: RichMember;
 
   namespaceOptions: string[] = [];
+  namespaceRules: NamespaceRules[] = [];
+  parsedRules: Map<string, {login: string, password: string}> =
+    new Map<string, {login: string, password: string}>();
 
   emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
 
@@ -41,6 +45,7 @@ export class CreateSponsoredMemberDialogComponent implements OnInit {
   titleAfter = '';
 
   passwordReset = false;
+  passwordResetDisabled = false;
   password = new FormControl('', [Validators.required]);
 
   namespace = new FormControl('', [Validators.required]);
@@ -54,27 +59,56 @@ export class CreateSponsoredMemberDialogComponent implements OnInit {
   constructor(private dialogRef: MatDialogRef<CreateSponsoredMemberDialogComponent>,
               @Inject(MAT_DIALOG_DATA) private data: CreateSponsoredMemberDialogData,
               private membersService: MembersManagerService,
-              private store: StoreService) {
+              private store: StoreService,
+              private translator: TranslateService) {
   }
 
   ngOnInit(): void {
     this.loading = true;
     this.theme = this.data.theme;
-    this.parseNamespace();
-    if (this.namespaceOptions.length === 0) {
-      this.functionalityNotSupported = true;
-    }
-    this.loading = false;
+
+    this.membersService.getAllNamespacesRules().subscribe(rules => {
+      if (this.store.get('allow_empty_sponsor_namespace')) {
+        this.namespaceRules.push({
+          namespaceName: 'No namespace',
+          requiredAttributes: [],
+          optionalAttributes: []
+        });
+      }
+
+      this.namespaceRules = this.namespaceRules.concat(rules);
+      this.parseNamespaceRules();
+      if (this.namespaceOptions.length === 0) {
+        this.functionalityNotSupported = true;
+      }
+      this.loading = false;
+    });
   }
 
-  parseNamespace(){
-    //TODO get login_namespace_attributes when creating for other namespaces will be available, then delete
-    //TODO sponsor_namespace_attributes from configuration
-    const namespaces = this.store.get("sponsor_namespace_attributes");
-    for (const namespace of namespaces) {
-      const index = namespace.lastIndexOf(':');
-      if (index !== -1) {
-        this.namespaceOptions.push(namespace.substring(index + 1, namespace.length));
+  parseNamespaceRules(){
+    for (const rule of this.namespaceRules) {
+      this.namespaceOptions.push(rule.namespaceName);
+
+      const fieldTypes =  {login: 'disabled', password: 'disabled'};
+      this.parseAttributes(fieldTypes, rule.requiredAttributes, 'required');
+      this.parseAttributes(fieldTypes, rule.optionalAttributes, 'optional');
+
+      this.parsedRules.set(rule.namespaceName, fieldTypes);
+    }
+  }
+
+  parseAttributes(field, attributes, type: string) {
+    for (const att of attributes) {
+      switch (att) {
+        case 'login': {
+          field.login = type;
+          break;
+        }
+        case 'password': {
+          field.password = type;
+          break;
+        }
+        default: break;
       }
     }
   }
@@ -89,13 +123,23 @@ export class CreateSponsoredMemberDialogComponent implements OnInit {
         lastName: this.lastName.value,
         titleAfter: this.titleAfter,
         titleBefore: this.titleBefore,
-        namespace: this.namespace.value,
-        password: this.passwordReset ? '' : this.password.value,
-        email: this.email.value,
-        login: this.login.value
+        email: this.email.value
       },
       sponsor: this.store.getPerunPrincipal().userId,
-      sendActivationLink: this.passwordReset
+    }
+
+    const rules = this.parsedRules.get(this.namespace.value);
+    if (this.namespace.value !== 'No namespace') {
+      sponsoredMember.userData.namespace = this.namespace.value;
+    }
+
+    if (rules.login !== 'disabled') {
+      sponsoredMember.userData.login = this.login.value;
+    }
+
+    if (rules.password !== 'disabled') {
+      sponsoredMember.sendActivationLink = this.passwordReset;
+      sponsoredMember.userData.password = this.password.value;
     }
 
     if(this.expiration !== 'never'){
@@ -130,17 +174,35 @@ export class CreateSponsoredMemberDialogComponent implements OnInit {
 
   }
 
-  changeNamespace(namespc: string) {
-    if (namespc === 'mu') {
-      this.login.disable();
-    } else {
-      this.login.enable();
+  enableFormControl(control: FormControl, validators: ValidatorFn[] ) {
+    control.enable();
+    control.clearValidators();
+    control.setValidators(validators);
+    control.updateValueAndValidity();
+  }
+
+  onNamespaceChanged(namespc: string) {
+    const rules = this.parsedRules.get(namespc);
+    this.login.disable();
+    this.password.disable();
+    this.passwordReset = false;
+    this.passwordResetDisabled = true;
+
+    if (rules.login !== 'disabled') {
+      const validators = rules.login === 'optional' ? [] : [Validators.required];
+      this.enableFormControl(this.login, validators);
+    }
+    if (rules.password !== 'disabled') {
+      const validators = rules.password === 'optional' ? [] : [Validators.required];
+      this.enableFormControl(this.password, validators);
+      this.passwordResetDisabled = false;
     }
   }
 
   passwordResetChange() {
     if (this.passwordReset){
       this.password.disable();
+      this.password.setValue('');
     } else {
       this.password.enable();
     }
@@ -154,4 +216,11 @@ export class CreateSponsoredMemberDialogComponent implements OnInit {
     }
   }
 
+  getPasswordDisabledTooltip(): string {
+    if (this.passwordReset) {
+      return this.translator.instant('DIALOGS.CREATE_SPONSORED_MEMBER.PASSWORD_VIA_EMAIL');
+    } else {
+      return this.translator.instant('DIALOGS.CREATE_SPONSORED_MEMBER.PASSWORD_DISABLED');
+    }
+  }
 }
