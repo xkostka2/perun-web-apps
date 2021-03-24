3cda89f5d597940edeaae7b51c6cf6dcd490c73b
Adam Bodnár <adam.bodnar10@gmail.com>
2021-03-24 14:00:11 +0100

    Password reset implementation
    * it is now possible to change password in Password reset app
    * added password strength check
    * added token validity check

--- apps/password-reset/src/app/dialogs/password-reset-dialog/password-reset-dialog.component.ts
+++ apps/password-reset/src/app/dialogs/password-reset-dialog/password-reset-dialog.component.ts
@@ -3,9 +3,15 @@ import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { UsersManagerService } from '@perun-web-apps/perun/openapi';
+import { ApiRequestConfigurationService } from '@perun-web-apps/perun/services';
+import { of, timer } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
 export interface PasswordResetDialogData {
-  mode: string
+  mode: string;
+  token?: string;
+  namespace?: string;
 }
 
 export class PasswordStateMatcher implements ErrorStateMatcher {
@@ -17,6 +23,34 @@ export class PasswordStateMatcher implements ErrorStateMatcher {
   }
 }
 
+/**
+ * State matcher that shows error on inputs whenever the input is changed and invalid (by default, the error
+ * is shown after leaving the input field)
+ */
+export class ImmediateStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return !!(control && control.invalid && control.dirty);
+  }
+}
+
+export const loginAsyncValidator =
+  (namespace: string, usersManager: UsersManagerService, apiRequestConfiguration: ApiRequestConfigurationService, time: number = 500) => {
+    return (input: FormControl) => {
+      return timer(time).pipe(
+        switchMap(() => {
+          apiRequestConfiguration.dontHandleErrorForNext();
+          if (namespace === null || namespace === 'No namespace') {
+            return of(null);
+          }
+          return usersManager.checkPasswordStrength(input.value, namespace)
+        }),
+        map(() => null),
+        // catch error and send it as a valid value
+        catchError(err => of({backendError: err.error.message.substr(err.error.message.indexOf(":")+1)})),
+      );
+    };
+  };
+
 @Component({
   selector: 'perun-web-apps-password-reset-dialog',
   templateUrl: './password-reset-dialog.component.html',
@@ -26,23 +60,30 @@ export class PasswordResetDialogComponent implements OnInit {
 
   constructor(
     @Inject(MAT_DIALOG_DATA) private data: PasswordResetDialogData,
-    private translate: TranslateService
-  ) { }
+    private translate: TranslateService,
+    private apiRequestConfiguration: ApiRequestConfigurationService,
+    private usersService: UsersManagerService) { }
 
   oldPasswd: FormControl = new FormControl('', Validators.required);
   newPasswdForm = new FormGroup({
-    password: new FormControl('', Validators.required),
-    passwordConfirm: new FormControl('', Validators.required)
+    password: new FormControl('', Validators.required,
+      [loginAsyncValidator(this.data.namespace, this.usersService, this.apiRequestConfiguration)]),
+    passwordConfirm: new FormControl('', Validators.required),
+    showPassword: new FormControl(false),
   }, this.passwordMatchValidator);
 
-  matcher = new PasswordStateMatcher();
+  passwordStateMatcher = new PasswordStateMatcher();
+  immediateStateMatcher = new ImmediateStateMatcher();
 
   mode = '';
   title = '';
 
-  ngOnInit(): void {
-    this.mode = this.data.mode;
+  loading = false;
+  success = false;
 
+  ngOnInit(): void {
+    this.loading = true;
+    this.mode = this.data.mode;
     switch (this.mode) {
       case 'change': {
         this.title = this.translate.instant('DIALOGS.PASSWORD_CHANGE.TITLE_CHANGE');
@@ -57,6 +98,7 @@ export class PasswordResetDialogComponent implements OnInit {
         break;
       }
     }
+    this.loading = false;
   }
 
   passwordMatchValidator(group: FormGroup) {
@@ -65,6 +107,10 @@ export class PasswordResetDialogComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log("Yey :)");
+    this.loading = true;
+    this.usersService.changeNonAuthzPasswordByToken(this.data.token, this.newPasswdForm.get('password').value).subscribe(() => {
+      this.success = true;
+      this.loading = false;
+    });
   }
 }
